2701f686ecb8befe7c4f0964ca22034589882013
Christopher Moura <ccmoura@inf.ufpel.edu.br>
2020-06-28 16:25:21 -0300

    done

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,16 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    if(balance.total >= value || type === 'income') {
+      const transaction = this.transactionsRepository.create({ title, value, type });
+
+      return transaction;
+    } else {
+      throw Error('Insufficient balance to complete the transaction');
+    }
   }
 }
 
